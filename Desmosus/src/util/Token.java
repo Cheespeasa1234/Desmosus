6b789ddf195fbf0f48da0fcd5056b52cc9cf7015
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2022-10-12 18:06:20 -0400

    make token class

---
+++ Desmosus/src/util/Token.java
@@ -0,0 +1,16 @@
+package Desmosus.src.processing;
+
+public class Token {
+
+    static char[] operators = {'+', '-', '*', '/', '(', ')'};
+
+    String value;
+    public Token(String value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Integer.parseInt("asodkaosd");
+    }
+
+}
