8b972ea87304d5fbcd125fe8829e600e23e52338
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2022-10-18 13:00:07 -0400

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,32 +1,60 @@
 package src;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Program {
     public static void main(String[] args) {
-        // System.out.println(new Desmosus("-1+2").equals());
-        int[] a = {1,3,4};
-        int b = a[3];
+        String fixed = "-1+2";
+
     } 
 
-    static boolean isOp(String token) {
+    static String[] tokenize(String eq) {
+        String curnum = "";
+        ArrayList<String> tokens = new ArrayList<String>();
+        for(int i = 0; i < eq.length(); i++) {
+            if(isNum(eq.charAt(i))) {
+                curnum+=eq.charAt(i);
+            } else {
+                tokens.add(curnum);
+                curnum = "";
+                tokens.add(eq.charAt(i));
+            }
+        }
+    }
+
+    static boolean isNum(char token) {
+        return "1234567890.".indexOf(token) > -1;
+    }
+
+    static boolean isNone(String[] tokens, int index) {
+        return index < 0 || index > tokens.length;
+    }
+    static boolean isOp(String[] tokens, int index) {
         String ops = "+-*/";
-        return ops.indexOf(token.charAt(0)) > -1;
+        return ops.indexOf(tokens[index].charAt(0)) > -1;
     }
 
     static String[] fixNegatives(String[] tokens) {
         for(int i = 0; i < tokens.length; i++) {
-            if(isOp(tokens[i])) {
-
-                if(isOp(1+"")){}
-
+            if(tokens[i] == " ") {
+                i++;
+            } else if(isOp(tokens, i) && (isNone(tokens, i-1) || isOp(tokens, i-1))) {
+                tokens = insert(tokens, "0", 0);
             }
         }
         return tokens;
     }
 
     static String[] insert(String[] list, String token, int index) {
-        // String[] aux = new String[] list.length
-        return list;
+        String[] aux = new String[list.length];
+        for(int i = 0; i < aux.length; i++) {
+            if(i == index) {
+                aux[i] = token;
+                i++;
+            } else
+                aux[i] =  list[i];
+        }
+        return aux;
     }
 }
